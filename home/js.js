5768f6cd6a09988492cca702d2f607a653c28715
Sneffel <9219144+Sneffel@users.noreply.github.com>
2022-01-19 00:58:52 +0100

    mobile

--- home/js.js
+++ home/js.js
@@ -19,3 +19,20 @@ function checkTime(i) {
 window.addEventListener("beforeunload", function () {
   document.body.classList.add("animate-out");
 });
+
+
+//
+//sotto per ridimensionare tabella
+//
+
+window.setMobileTable = function(selector) {
+  // if (window.innerWidth > 600) return false;
+  const tableEl = document.querySelector(selector);
+  const thEls = tableEl.querySelectorAll('thead th');
+  const tdLabels = Array.from(thEls).map(el => el.innerText);
+  tableEl.querySelectorAll('tbody tr').forEach( tr => {
+    Array.from(tr.children).forEach(
+      (td, ndx) =>  td.setAttribute('label', tdLabels[ndx])
+    );
+  });
+}

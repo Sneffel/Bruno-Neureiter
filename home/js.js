3dd680b9a23b564c657a34093f38829ee02086d5
Sneffel <9219144+Sneffel@users.noreply.github.com>
2022-01-18 23:11:31 +0100

    animazioni

--- home/js.js
+++ home/js.js
@@ -15,3 +15,7 @@ function checkTime(i) {
   if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
   return i;
 }
+
+window.addEventListener("beforeunload", function () {
+  document.body.classList.add("animate-out");
+});
